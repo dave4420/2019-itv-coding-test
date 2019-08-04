91c07d368c63fff9aab95bcf50005a58535b58ad
Dave Hinton <dave@4420.uk>
2019-08-04 15:49:10 +0100

    generate thumbnail, by shelling out to ffmpeg

--- src/main/scala/uk/_4420/ThumbnailGenerator.scala
+++ src/main/scala/uk/_4420/ThumbnailGenerator.scala
@@ -2,6 +2,18 @@ package uk._4420
 
 import java.io.File
 
+import scala.sys.process._
+
 class ThumbnailGenerator(ffmpegExecutableName: String) {
-  def generateThumbnail(inputVideoFile: File, outputThumbnailFile: File): Unit = {} // DAVE
+  def generateThumbnail(inputVideoFile: File, outputThumbnailFile: File): Unit = {
+    Seq(
+      ffmpegExecutableName,
+      "-i", inputVideoFile.getPath,
+      "-vframes", "1", // output one frame
+      "-an", // disable audio
+      // -s 400x222 output size
+      "-ss", "2", // offset into video to grab thumbnail from (seconds)
+      outputThumbnailFile.getPath,
+    ).!
+  }
 }
